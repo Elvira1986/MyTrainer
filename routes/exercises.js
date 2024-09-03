6d842ce30f3d6d6778330e412c092a3ef427c620
w-sihara <136378346+w-sihara@users.noreply.github.com>
2024-09-03 19:56:33 +0530

    Add GET /exercises endpoint to retrieve all exercises

--- routes/exercises.js
+++ routes/exercises.js
@@ -6,4 +6,14 @@ const db = require("../model/helper");
 
 // Show ALL exercises: http://localhost:4000/api/exercises/
 
+// Get all exercises
+router.get('/', async (req, res) => {
+    try {
+        const exercises = await db.query('SELECT * FROM exercises');
+        res.status(200).json(exercises);
+    } catch (err) {
+        res.status(500).json({ error: 'Database query failed' });
+    }
+});
+
 module.exports = router;
