faaad737927d0904a16e77d282bb4f21fb5a8cd5
BeliMuga <belindamuga55@gmail.com>
2024-09-09 15:28:29 +0200

    Get, post and delete fav food working

--- routes/favfoods.js
+++ routes/favfoods.js
@@ -8,39 +8,40 @@ const userShouldBeLoggedIn = require("../guards/userShouldBeLoggedIn");
 // Show all favorite_food: http://localhost:4000/api/favfoods/
 
 // Get all favorite foods
-router.get('/food/:id', userShouldBeLoggedIn, async (req, res) => {
+router.get('/food',userShouldBeLoggedIn, async (req, res) => {
+    const SELECT = `SELECT id, name, image, external_api_id FROM favorite_food WHERE users_id = ${req.user_id};`;
+    
     try {
-        const result = await db.query(`SELECT * FROM favorite_food WHERE users_id = ${req.user_id}`);
-        if (result.data.length === 1) {
-            // food is found!
-            res.send(result.data[0]); // 
-        } else {
-            // food not found!
-            res.status(404).send({ error: "books not found" });
-        }
+        const result = await db(SELECT);
+        res.send(result.data)
     } catch (err) {
-        res.status(500).json({ error: 'Database query failed' });
+        res.status(500).send({ error: 'Database query failed' });
     }
 });
 
 // Add a new favorite food
-router.post('/food', userShouldBeLoggedIn, async (req, res) => {
-    const { externalApiId, name, image } = req.body;
+router.post('/food',userShouldBeLoggedIn, async (req, res) => {
+    const { external_api_id, name, image } = req.body;
+    console.log(req.user_id)
     try {
-        await db.query(`INSERT INTO favorite_food (users_id, external_api_id, name, image) VALUES ${req.user_id}, ${externalApiId}, "${name}", "${image}"`);
-        res.status(201).json({ message: 'Favorite food added' });
+        await db(`INSERT INTO favorite_food (users_id, external_api_id, name, image) VALUES (${req.user_id}, '${external_api_id}', '${name}', '${image}');`);
+        console.log(external_api_id, name, image)
+        res.status(200).send({ message: 'Favorite food added' });
     } catch (err) {
-        res.status(500).json({ error: 'Database query failed' });
+        res.status(500).send({ error: 'Database query failed' });
     }
 });
 
-// Delete a favorite food by ID
-router.delete("/food/:id", userShouldBeLoggedIn, async (req, res) => {
-    try {
-    await db(`SELECT * FROM favorite_food WHERE id = ${req.params.id} AND users_id = ${req.user_id}`);
-    await db(`DELETE FROM favorite_food WHERE id = ${req.params.id} AND users_id = ${req.user_id}`);
 
-    const result = await db(`SELECT * FROM ${req.user_id}`)
+
+// Delete a favorite food by ID
+router.delete("/food", userShouldBeLoggedIn, async (req, res) => {
+    const { external_api_id } = req.body;
+    try {
+    await db(`SELECT * FROM favorite_food WHERE external_api_id = "${external_api_id}" AND users_id = ${req.user_id};`);
+    await db(`DELETE FROM favorite_food WHERE external_api_id = "${external_api_id}" AND users_id = ${req.user_id};`);
+
+    const result = await db(`SELECT * FROM favorite_food WHERE users_id = ${req.user_id};`)
     res.send(result.data)
 
     } catch (err) {
